76a17c601f64036dc1a473a3c18e20dfea3f9d9e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2020-10-15 19:43:17 -0300

    Alterando classe Funcionario na parte 9 da aula 1 do curso Kotlin: herança, polimorfismo e Interface

--- bytebank/src/Funcionario.kt
+++ bytebank/src/Funcionario.kt
@@ -1,14 +1,32 @@
 /**
  * Representa um funcionario do Bytebank.
+ * @param nome Nome do funcionario.
+ * @param cpf Cpf do funcionario.
+ * @param salario Salario do funcionario.
+ * @param tipo Tipo do funcionario: 0 - Funcionario comum; 1- Gerente; 2- Diretor.
  * */
-class Funcionario(val nome: String, val cpf: String, val salario: Double) {
+class Funcionario(val nome: String, val cpf: String, val salario: Double, val tipo: Int = 0) {
 
     /**
      * Serve para calcular a bonificacao de uma instancia da classe Funcionario.
      * @return Valor da bonificacao do funcionario.
      * */
     fun bonificacao(): Double {
-        return this.salario * 0.10
+        var bonificacao: Double = 0.0;
+
+        when (this.tipo)
+        {
+            0 -> {
+                bonificacao = this.salario * 0.1
+            }
+            1 -> {
+                bonificacao = this.salario * 0.2
+            }
+            3 -> {
+                bonificacao = this.salario * 0.3
+            }
+        }
+        return bonificacao
     }
 
     /**
@@ -19,7 +37,8 @@ class Funcionario(val nome: String, val cpf: String, val salario: Double) {
         var str = ""
         str += "Nome: " + this.nome + "\n"
         str += "CPF: " + this.cpf + "\n"
-        str += "Salario: " + this.salario
+        str += "Salario: " + this.salario + "\n"
+        str += "Tipo: " + this.tipo
 
         return str
     }
