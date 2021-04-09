b5afed178023c4f206b9e25bad960b2ea6af4d9a
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-04-09 19:00:30 -0300

    Parte 5 da aula 1 do curso Kotlin: lidando com exceptions e referências nulas

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,5 +1,19 @@
-import teste.testaAny
-
 fun main() {
-    testaAny()
+    println("início main")
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início função1")
+    funcao2()
+    println("fim função1")
+}
+
+fun funcao2() {
+    println("início função2")
+    for (i in 1..5){
+        println(i)
+    }
+    println("fim função2")
 }
