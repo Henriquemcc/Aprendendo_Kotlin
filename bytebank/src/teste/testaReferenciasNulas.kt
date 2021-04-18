b2daf59f8b260d767a70acc925594b8d2b78fbd3
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-04-18 15:57:34 -0300

    Parte 4 da aula 5 do curso Kotlin: lidando com exceptions e referências nulas

--- bytebank/src/teste/testaReferenciasNulas.kt
+++ bytebank/src/teste/testaReferenciasNulas.kt
@@ -3,6 +3,18 @@ package teste
 import modelo.Endereco
 
 fun testaReferenciasNulas() {
-    val endereco: Endereco? = null
-    endereco!!.numero
+
+    //extraia o valor do logradouro para outra variável chamando-o por meio da safe call
+    val endereco: Endereco? = Endereco(logradouro = "Rua 1")
+    val logradouro = endereco?.logradouro
+    println(logradouro)
+
+    //faça chamadas encadeadas utilizando o safe call;
+    println(endereco?.logradouro?.length)
+
+    //utilize a combinação da função de escopo let.
+    endereco?.let {
+        println(it.logradouro.hashCode())
+    }
+
 }
