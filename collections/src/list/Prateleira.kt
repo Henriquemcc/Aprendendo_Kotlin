83b5339b897a08c60b2fe30abfc776024dc574ee
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-04-30 15:21:21 -0300

    Parte 7.1 da aula 5 do curso Kotlin: Desenvolva com coleções, arrays e listas.

--- collections/src/list/Prateleira.kt
+++ collections/src/list/Prateleira.kt
@@ -1,16 +1,28 @@
 package list
 
+/**
+ * Representa uma prateleira de livros
+ * @param genero Gênero da prateleira de livros
+ * @param livros Lista de livros da prateleira
+ */
 data class Prateleira(
     val genero: String,
-    val livros: MutableList<Livro>
+    val livros: List<Livro>
 ) {
-    fun organizarLivrosPorAutor(): MutableList<Livro> {
-        this.livros.sortBy { it.autor }
-        return this.livros
+    /**
+     * Obtém uma lista de livros ordenada por autor
+     * @return Lista imutável de livros
+     */
+    fun organizarLivrosPorAutor(): List<Livro> {
+        return this.livros.sortedBy { it.autor }
+
     }
 
-    fun organizarLivrosPorAnoPublicacao(): MutableList<Livro> {
-        this.livros.sortBy { it.anoPublicacao }
-        return this.livros
+    /**
+     * Obtém uma lista de livros ordenada por ano de publicação
+     * @return Lista imutável de livros
+     */
+    fun organizarLivrosPorAnoPublicacao(): List<Livro> {
+        return this.livros.sortedBy { it.anoPublicacao }
     }
 }
