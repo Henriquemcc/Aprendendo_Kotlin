c05c7c5e17c3b088447fdd0ab548b71ff23a9c1c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-05-10 15:05:15 -0300

    Parte 4 da aula 2 do curso Kotlin Collections: Set e Map.

--- collections set e map/src/main.kt
+++ collections set e map/src/main.kt
@@ -1,8 +1,3 @@
-fun main() {
-
-    testarDistinct()
-}
-
 /**
  * Testa o comportamento do método distinct de uma lista.
  */
@@ -20,8 +15,112 @@ private fun testarDistinct() {
     val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
     println("Assistiram ambos os cursos: $assistiramAmbos")
     println()
+
     println("Assistiram ambos os cursos (com distinct): ${assistiramAmbos.distinct()}")
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa o comportamento do operador de soma de list com set.
+ */
+private fun testarOperadorSomaListComSet() {
+    println("Testando o operador de soma de list com set")
+
+    val assistiramCursoAndroid = setOf("Alex", "Fran", "Gui", "Maria")
+    println("Assistiram o curso de Android: $assistiramCursoAndroid")
+    println()
+
+    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
+    println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
+    println()
+
+    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram ambos os cursos: $assistiramAmbos")
+    println()
+
+    println("Assistiram ambos os cursos (com distinct): ${assistiramAmbos.distinct()}")
+    println()
+
+    println("Assistiram ambos posição 0: ${assistiramAmbos[0]}")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa o comportamento do operador de soma de set com list.
+ */
+private fun testarOperadorSomaSetComList() {
+    println("Testando o operador de soma de list com set")
+
+    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
+    println("Assistiram o curso de Android: $assistiramCursoAndroid")
+    println()
+
+    val assistiramCursoKotlin = setOf("Alex", "Paulo", "Maria")
+    println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
+    println()
+
+    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram ambos os cursos: $assistiramAmbos")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa o comportamento do operador de soma de set com list.
+ */
+private fun testarOperadorSomaSet() {
+    println("Testando o operador de soma de set")
+
+    val assistiramCursoAndroid = setOf("Alex", "Fran", "Gui", "Maria")
+    println("Assistiram o curso de Android: $assistiramCursoAndroid")
+    println()
+
+    val assistiramCursoKotlin = setOf("Alex", "Paulo", "Maria")
+    println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
+    println()
+
+    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram ambos os cursos: $assistiramAmbos")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa o comportamento do operador de soma de list com set.
+ */
+private fun testarOperadorSomaMutableSet() {
+    println("Testando o operador de soma de mutable set")
+
+    val assistiramCursoAndroid = mutableSetOf("Alex", "Fran", "Gui", "Maria")
+    println("Assistiram o curso de Android: $assistiramCursoAndroid")
+    println()
+
+    val assistiramCursoKotlin = mutableSetOf("Alex", "Paulo", "Maria")
+    println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
+    println()
+
+    val assistiramAmbos = mutableSetOf<String>()
+    assistiramAmbos.addAll(assistiramCursoAndroid)
+    assistiramAmbos.addAll(assistiramCursoKotlin)
+    assistiramAmbos.add("Ana")
+    assistiramAmbos.add("Ana")
+    println("Assistiram ambos os cursos: $assistiramAmbos")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+fun main() {
+
+    testarDistinct()
+    testarOperadorSomaListComSet()
+    testarOperadorSomaSetComList()
+    testarOperadorSomaSet()
+    testarOperadorSomaMutableSet()
 }
