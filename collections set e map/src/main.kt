cd7d433591983cdb4777d851c28f887ffd098094
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-05-10 15:44:31 -0300

    Parte 6 da aula 2 do curso Kotlin Collections: Set e Map.

--- collections set e map/src/main.kt
+++ collections set e map/src/main.kt
@@ -3,6 +3,7 @@
  */
 private fun testarDistinct() {
     println("Testando o método distinct de uma lista")
+    println()
 
     val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
     println("Assistiram o curso de Android: $assistiramCursoAndroid")
@@ -12,11 +13,11 @@ private fun testarDistinct() {
     println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
     println()
 
-    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
-    println("Assistiram ambos os cursos: $assistiramAmbos")
+    val assistiramPeloMenosUmDosCursos = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram pelo menos um dos cursos: $assistiramPeloMenosUmDosCursos")
     println()
 
-    println("Assistiram ambos os cursos (com distinct): ${assistiramAmbos.distinct()}")
+    println("Assistiram pelo menos um dos cursos (com distinct): ${assistiramPeloMenosUmDosCursos.distinct()}")
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
@@ -27,6 +28,7 @@ private fun testarDistinct() {
  */
 private fun testarOperadorSomaListComSet() {
     println("Testando o operador de soma de list com set")
+    println()
 
     val assistiramCursoAndroid = setOf("Alex", "Fran", "Gui", "Maria")
     println("Assistiram o curso de Android: $assistiramCursoAndroid")
@@ -36,14 +38,14 @@ private fun testarOperadorSomaListComSet() {
     println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
     println()
 
-    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
-    println("Assistiram ambos os cursos: $assistiramAmbos")
+    val assistiramPeloMenosUmDosCursos = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram pelo menos um dos cursos: $assistiramPeloMenosUmDosCursos")
     println()
 
-    println("Assistiram ambos os cursos (com distinct): ${assistiramAmbos.distinct()}")
+    println("Assistiram pelo menos um dos cursos (com distinct): ${assistiramPeloMenosUmDosCursos.distinct()}")
     println()
 
-    println("Assistiram ambos posição 0: ${assistiramAmbos[0]}")
+    println("Assistiram ambos posição 0: ${assistiramPeloMenosUmDosCursos[0]}")
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
@@ -54,6 +56,7 @@ private fun testarOperadorSomaListComSet() {
  */
 private fun testarOperadorSomaSetComList() {
     println("Testando o operador de soma de list com set")
+    println()
 
     val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
     println("Assistiram o curso de Android: $assistiramCursoAndroid")
@@ -63,8 +66,8 @@ private fun testarOperadorSomaSetComList() {
     println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
     println()
 
-    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
-    println("Assistiram ambos os cursos: $assistiramAmbos")
+    val assistiramPeloMenosUmDosCursos = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram pelo menos um dos cursos: $assistiramPeloMenosUmDosCursos")
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
@@ -75,6 +78,7 @@ private fun testarOperadorSomaSetComList() {
  */
 private fun testarOperadorSomaSet() {
     println("Testando o operador de soma de set")
+    println()
 
     val assistiramCursoAndroid = setOf("Alex", "Fran", "Gui", "Maria")
     println("Assistiram o curso de Android: $assistiramCursoAndroid")
@@ -84,8 +88,8 @@ private fun testarOperadorSomaSet() {
     println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
     println()
 
-    val assistiramAmbos = assistiramCursoKotlin + assistiramCursoAndroid
-    println("Assistiram ambos os cursos: $assistiramAmbos")
+    val assistiramPeloMenosUmDosCursos = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram pelo menos um dos cursos: $assistiramPeloMenosUmDosCursos")
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
@@ -96,6 +100,7 @@ private fun testarOperadorSomaSet() {
  */
 private fun testarOperadorSomaMutableSet() {
     println("Testando o operador de soma de mutable set")
+    println()
 
     val assistiramCursoAndroid = mutableSetOf("Alex", "Fran", "Gui", "Maria")
     println("Assistiram o curso de Android: $assistiramCursoAndroid")
@@ -105,12 +110,134 @@ private fun testarOperadorSomaMutableSet() {
     println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
     println()
 
-    val assistiramAmbos = mutableSetOf<String>()
-    assistiramAmbos.addAll(assistiramCursoAndroid)
-    assistiramAmbos.addAll(assistiramCursoKotlin)
-    assistiramAmbos.add("Ana")
-    assistiramAmbos.add("Ana")
-    println("Assistiram ambos os cursos: $assistiramAmbos")
+    val assistiramPeloMenosUmDosCursos = mutableSetOf<String>()
+    assistiramPeloMenosUmDosCursos.addAll(assistiramCursoAndroid)
+    assistiramPeloMenosUmDosCursos.addAll(assistiramCursoKotlin)
+    assistiramPeloMenosUmDosCursos.add("Ana")
+    assistiramPeloMenosUmDosCursos.add("Ana")
+    println("Assistiram pelo menos um dos cursos: $assistiramPeloMenosUmDosCursos")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa o comportamento do operador union do set.
+ */
+private fun testarOperadorUnionSet() {
+    println("Testando o operador union de set")
+    println()
+
+    val assistiramCursoAndroid = setOf("Alex", "Fran", "Gui", "Maria")
+    println("Assistiram o curso de Android: $assistiramCursoAndroid")
+    println()
+
+    val assistiramCursoKotlin = setOf("Alex", "Paulo", "Maria")
+    println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
+    println()
+
+    val assistiramPeloMenosUmDosCursosSoma = assistiramCursoKotlin + assistiramCursoAndroid
+    println("Assistiram pelo menos um dos cursos (soma): $assistiramPeloMenosUmDosCursosSoma")
+    println()
+
+    val assistiramPeloMenosUmDosCursosUnion = assistiramCursoKotlin union assistiramCursoAndroid
+    println("Assistiram pelo menos um dos cursos (union): $assistiramPeloMenosUmDosCursosUnion")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa o comportamento do operador de subtração do set.
+ */
+private fun testarOperadorSubtracaoSet() {
+    println("Testando o operador subtração de set")
+    println()
+
+    val assistiramCursoAndroid = setOf("Alex", "Fran", "Gui", "Maria")
+    println("Assistiram o curso de Android: $assistiramCursoAndroid")
+    println()
+
+    val assistiramCursoKotlin = setOf("Alex", "Paulo", "Maria")
+    println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
+    println()
+
+    val subtracaoAssistiramCursoAndroidMenosKotlin = assistiramCursoAndroid - assistiramCursoKotlin
+    println("Assistiram o curso de Android - Assistiram o curso de Kotlin: $subtracaoAssistiramCursoAndroidMenosKotlin")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa o comportamento do operador intersect do set.
+ */
+private fun testarOperadorIntersectSet() {
+    println("Testando o operador intersect de set")
+    println()
+
+    val assistiramCursoAndroid = setOf("Alex", "Fran", "Gui", "Maria")
+    println("Assistiram o curso de Android: $assistiramCursoAndroid")
+    println()
+
+    val assistiramCursoKotlin = setOf("Alex", "Paulo", "Maria")
+    println("Assistiram o curso de Kotlin: $assistiramCursoKotlin")
+    println()
+
+    val assistiramCursosKotlinEAndroid = assistiramCursoKotlin intersect assistiramCursoAndroid
+    println("Assistiram curso de kotlin e curso de Android: $assistiramCursosKotlinEAndroid")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa a conversão de set para list.
+ */
+private fun testarConversaoSetParaList() {
+    println("Testando a conversão de set para list")
+    println()
+
+    val assistiramCursosSet = setOf("Alex", "Fran", "Gui", "Maria", "Paulo")
+    println("Assistiram cursos (set): $assistiramCursosSet")
+    println()
+
+    val assistiramCursosMutableList = assistiramCursosSet.toMutableList()
+    println("Assistiram cursos (mutable list): $assistiramCursosMutableList")
+    println()
+
+    assistiramCursosMutableList.add("Ana")
+    println("Assistiram cursos (mutable list) + 'Ana': $assistiramCursosMutableList")
+    println()
+
+    assistiramCursosMutableList.add("Ana")
+    println("Assistiram cursos (mutable list) + 'Ana' + 'Ana': $assistiramCursosMutableList")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+/**
+ * Testa a conversão de list para set.
+ */
+private fun testarConversaoListParaSet() {
+    println("Testando a conversão de set para list")
+    println()
+
+    val assistiramCursosSet = listOf("Alex", "Fran", "Gui", "Maria", "Paulo")
+    println("Assistiram cursos (list): $assistiramCursosSet")
+    println()
+
+    val assistiramCursosMutableList = assistiramCursosSet.toMutableSet()
+    println("Assistiram cursos (mutable set): $assistiramCursosMutableList")
+    println()
+
+    assistiramCursosMutableList.add("Ana")
+    println("Assistiram cursos (mutable set) + 'Ana': $assistiramCursosMutableList")
+    println()
+
+    assistiramCursosMutableList.add("Ana")
+    println("Assistiram cursos (mutable set) + 'Ana' + 'Ana': $assistiramCursosMutableList")
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
@@ -123,4 +250,9 @@ fun main() {
     testarOperadorSomaSetComList()
     testarOperadorSomaSet()
     testarOperadorSomaMutableSet()
+    testarOperadorUnionSet()
+    testarOperadorSubtracaoSet()
+    testarOperadorIntersectSet()
+    testarConversaoSetParaList()
+    testarConversaoListParaSet()
 }
