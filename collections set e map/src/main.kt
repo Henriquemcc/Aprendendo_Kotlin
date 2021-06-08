487bc226a50fef9b91ee9e254f5a0ff56671fa9e
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-06-08 10:01:59 -0300

    Parte 4 da aula 5 do curso Kotlin Collections: Set e Map.

--- collections set e map/src/main.kt
+++ collections set e map/src/main.kt
@@ -1,7 +1,10 @@
 fun main() {
     testarMetodoAssociate()
-    testarMetodoAssociateBy()
+    testarMetodoAssociateByComONumeroDoPedido()
     testarMetodoAssociateWith()
+    testarMetodoAssociateByComOValorBooleanoIndicandoSeTemFreteGratis()
+    testarMetodoGroupBy()
+    testarMetodoGroupByAgenda()
 }
 
 private data class Pedido(val numero: Int, val valor: Double)
@@ -29,9 +32,9 @@ private fun testarMetodoAssociate() {
     println("---------------------------------------------------------------------------------------------------------------")
 }
 
-private fun testarMetodoAssociateBy() {
+private fun testarMetodoAssociateByComONumeroDoPedido() {
 
-    println("Testando o método associateBy")
+    println("Testando o método associateBy com o número do pedido")
     println()
 
     val pedidos = listOf(
@@ -80,6 +83,88 @@ private fun testarMetodoAssociateWith() {
     pedidoProcurado = Pedido(2, 60.0)
     pedidoProcuradoEstaEmPedidosComFreteGratis = pedidosComFreteGratis[pedidoProcurado]
     println("$pedidoProcurado está em pedidos com frete gratis: $pedidoProcuradoEstaEmPedidosComFreteGratis")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+private fun testarMetodoAssociateByComOValorBooleanoIndicandoSeTemFreteGratis() {
+
+    println("Testando o método associateBy com o valor booleano indicando se tem frete grátis")
+    println()
+
+    val pedidos = listOf(
+            Pedido(1, 20.0),
+            Pedido(2, 60.0),
+            Pedido(3, 30.0),
+            Pedido(4, 70.0)
+    )
+    println("Pedidos: $pedidos")
+    println()
+
+    val pedidosComFreteGratis = pedidos.associateBy { pedido ->
+        pedido.valor > 50.0
+    }
+    println("Pedidos com frete grátis: $pedidosComFreteGratis")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+private fun testarMetodoGroupBy() {
+
+    println("Testando o método groupBy")
+    println()
+
+    val pedidos = listOf(
+            Pedido(1, 20.0),
+            Pedido(2, 60.0),
+            Pedido(3, 30.0),
+            Pedido(4, 70.0)
+    )
+    println("Pedidos: $pedidos")
+    println()
+
+    val pedidosFreteGratisAgrupados = pedidos.groupBy { pedido ->
+        pedido.valor > 50.0
+    }
+    println("Pedidos agrupados de acordo com a gratuidade do frete: $pedidosFreteGratisAgrupados")
+    println()
+
+    println("Todos os pedidos cujo frete é gratuito: ${pedidosFreteGratisAgrupados[true]}")
+    println()
+
+    println("Todos os pedidos cujo frete não é gratuito: ${pedidosFreteGratisAgrupados[false]}")
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+private fun testarMetodoGroupByAgenda() {
+
+    println("Testando o método groupBy em uma agenda")
+    println()
+
+    val nomes = listOf(
+            "Alex",
+            "Fran",
+            "Gui",
+            "Ana",
+            "Paulo",
+            "Maria",
+            "Mario",
+            "Gisele"
+    )
+    println("Nomes: $nomes")
+    println()
+
+    val agenda = nomes.groupBy { nome ->
+        nome.first()
+    }
+    println("Agenda: $agenda")
+    println()
+
+    println("Nomes da agenda que começam com 'A': ${agenda['A']}")
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
