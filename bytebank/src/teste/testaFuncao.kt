b3ae9839579b2e20c9a30a225c7b654e959b320c
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-06-11 17:23:01 -0300

    Parte 8 da aula 1 do curso Kotlin: recursos do paradigma funcional.

--- bytebank/src/teste/testaFuncao.kt
+++ bytebank/src/teste/testaFuncao.kt
@@ -1,30 +1,77 @@
 package teste
 
 fun main() {
+    testarAtribuicaoFuncaoParaVariavel()
+    testarAtribuicaoClasseFuncaoParaVariavel()
+    testarAtribuicaoClasseFuncaoParaVariavelComInvokeComoOperator()
+}
+
+private fun testarAtribuicaoClasseFuncaoParaVariavelComInvokeComoOperator()
+{
+    println("Testando a atribuição de uma classe função com o invoke como operator para uma variável")
+    println()
+
+    val classeFuncaoTeste = :: Teste1
+    println("classeFuncaoTeste: ")
+    println(classeFuncaoTeste)
+    println()
+
+    println("Será executado o construtor da classeFuncaoTeste: ")
+    val instanciaClasseFuncaoTeste = classeFuncaoTeste()
+    println()
+
+    println("Instância da classeFuncaoTeste: ")
+    println(instanciaClasseFuncaoTeste)
+    println()
+
+    println("Será executado o operador invoke da classeFuncaoTeste: ")
+    println(instanciaClasseFuncaoTeste(0))
+    println()
+
+    println("Será executado o método invoke da classeFuncaoTeste: ")
+    println(instanciaClasseFuncaoTeste.invoke())
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+private fun testarAtribuicaoClasseFuncaoParaVariavel() {
+
+    println("Testando a atribuição de uma classe função para uma variável")
+    println()
+
+    val classeFuncaoTeste = ::Teste0
+    println("classeFuncaoTeste: ")
+    println(classeFuncaoTeste)
+    println()
+
+    println("Será executado o construtor da classeFuncaoTeste: ")
+    val instanciaClasseFuncaoTeste = classeFuncaoTeste()
+    println()
+
+    println("Instância da classeFuncaoTeste: ")
+    println(instanciaClasseFuncaoTeste)
+    println()
+
+    println("Será executado o método invoke da classeFuncaoTeste: ")
+    println(instanciaClasseFuncaoTeste.invoke())
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
+}
+
+private fun testarAtribuicaoFuncaoParaVariavel() {
 
     println("Testando a atribuição de uma função para uma variável")
     println()
 
     val funcaoTeste = :: teste
-    print("Valor da variável funcaoTeste: ")
+    println("Valor da variável funcaoTeste: ")
     println(funcaoTeste)
     println()
 
-    print("Executando a funcaoTeste: ")
+    println("Será executado a funcaoTeste: ")
     println(funcaoTeste())
-    println()
-
-    val classeFuncaoTeste = ::Teste
-    print("classeFuncaoTeste: ")
-    println(classeFuncaoTeste)
-    println()
-
-    print("Executando a classeFuncaoTeste: ")
-    println(classeFuncaoTeste())
-    println()
-
-    print("Executando o método invoke da classeFuncaoTeste: ")
-    println(classeFuncaoTeste().invoke())
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
@@ -32,13 +79,22 @@ fun main() {
 
 private fun teste() {
     println("Executando a função teste")
-    println()
 }
 
-private class Teste: () -> Unit {
+private class Teste0: () -> Unit {
     override fun invoke() {
-        println("Executando o método invoke da classe Teste que herda uma função '() -> Unit'")
-        println()
+        println("Executando o método invoke da classe Teste0 que herda uma função '() -> Unit'")
     }
 
+}
+
+private class Teste1: () -> Unit {
+    override fun invoke() {
+        println("Executando o método invoke da classe Teste1 que herda uma função '() -> Unit'")
+    }
+
+    operator fun invoke(valor: Int) {
+        println("Executando o operator invoke da classe Teste1 que herda uma função '() -> Unit'")
+        println(valor)
+    }
 }
