5de3fd382154edac40218f2b5d23a0b2a85b5d16
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-06-14 20:37:09 -0300

    Parte 4 da aula 4 do curso Kotlin: recursos do paradigma funcional.

--- bytebank/src/teste/testaFuncaoEscopo.kt
+++ bytebank/src/teste/testaFuncaoEscopo.kt
@@ -4,18 +4,46 @@ import modelo.Endereco
 
 fun main() {
 
-    testarFuncoesDeEscopoComObjetoDeContextoReceptor()
+    testarFuncoesDeEscopoParaGerarAStringEnderecoComLogradouroENumeroEmMaiusculo()
 }
 
-private fun testarFuncoesDeEscopoComObjetoDeContextoReceptor() {
+private fun testarFuncoesDeEscopoParaGerarAStringEnderecoComLogradouroENumeroEmMaiusculo() {
 
-    println("Testando as funções de escopo com objeto de contexto receptor")
+    println("Testando as funções de escopo para gerar a string de Endereço com o logradouro e o número em maiúsculo.")
+    println()
+
+    val endereco = Endereco("rua vergueiro", numero = 3185)
+
+    println("Função let:")
+    endereco.let {
+        println("A função let retorna o resultado da função lambda, que neste caso é uma string com o logradouro e o número, tudo em maiúsculo.")
+        "${it.logradouro}, ${it.numero}".toUpperCase()
+    }.let(::println)
     println()
 
     println("Função apply:")
-    Endereco("rua vergueiro", numero = 3185).apply {
+    endereco.apply {
+        println("A função apply retorna o objeto de contexto receptor, que neste caso é o objeto Endereco. Por esse motivo a string com o logradouro e o número, tudo em maiúsculo, não será impressa.")
         "$logradouro, $numero".toUpperCase()
     }.let(::println)
+    println()
+
+    println("Função run:")
+    endereco.run {
+        println("A função run retorna o resultado da função lambda, que neste caso é uma string com o logradouro e o número, tudo em maiúsculo.")
+        "$logradouro, $numero".toUpperCase()
+    }.let { stringEnderecoEmMaiusculo: String ->
+        println(stringEnderecoEmMaiusculo)
+    }
+    println()
+
+    println("Função with:")
+    with(endereco) {
+        println("A função with retorna o resultado da função lambda, que neste caso é uma string com o logradouro e o número, tudo em maiúsculo.")
+        "$logradouro, $numero".toUpperCase()
+    }.let { stringEnderecoEmMaiusculo: String ->
+        println(stringEnderecoEmMaiusculo)
+    }
     println()
 
     println("---------------------------------------------------------------------------------------------------------------")
