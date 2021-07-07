3a69a41c3af62a7d569b3bb3350590309d394ee2
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2021-07-07 12:43:56 -0300

    Parte 2 da aula 5 do curso Kotlin: recursos do paradigma funcional.

--- bytebank/src/main.kt
+++ bytebank/src/main.kt
@@ -1,15 +1,29 @@
-﻿import teste.testaReferenciasNulas
+﻿import modelo.Endereco
 
 fun main() {
-//    testaAny()
-//    testaAutenticacao()
-//    testaComportamentosConta()
-//    testaContasDiferentes()
-//    testaCopiasEReferencias()
-//    testaExcecoes()
-//    testaExpressao()
-//    testaFuncionarios()
-//    testaLacos()
-//    testaObjects()
-    testaReferenciasNulas()
+    testaWith()
+}
+
+fun testaWith() {
+
+    println("Testando a função de escopo with ")
+    println()
+
+    println("Endereço Completo")
+    with(Endereco())
+    {
+        logradouro = "rua vergueiro"
+        numero = 3185
+        bairro = "Villa Mariana"
+        cidade = "São Paulo"
+        estado = "SP"
+        cep = "02310-063"
+        complemento = "Apartamento"
+        completo()
+    }.let { enderecoCompleto ->
+        println(enderecoCompleto)
+    }
+    println()
+
+    println("---------------------------------------------------------------------------------------------------------------")
 }
